afdf8a81a3835f1a2e8ab4ab59ff23424ae9889b
ocefpaf <ocefpaf@gmail.com>
2016-02-09 13:22:20 -0300

    Python 3

--- cc_plugin_glider/tests/test_glidercheck.py
+++ cc_plugin_glider/tests/test_glidercheck.py
@@ -1,14 +1,23 @@
-from cc_plugin_glider.glider_dac import GliderCheck
+from __future__ import (absolute_import, division, print_function)
+
+import unittest
 from pkg_resources import resource_filename
 from netCDF4 import Dataset
-import unittest
+
+from ..glider_dac import GliderCheck
+
+try:
+    basestring
+except NameError:
+    basestring = str
 
 static_files = {
-    'glider_std': resource_filename('cc_plugin_glider', 'tests/data/gliders/IOOS_Glider_NetCDF_v2.0.nc'),
-    'bad_location': resource_filename('cc_plugin_glider', 'tests/data/gliders/bad_location.nc'),
-    'bad_qc': resource_filename('cc_plugin_glider', 'tests/data/gliders/bad_qc.nc'),
-    'bad_metadata': resource_filename('cc_plugin_glider', 'tests/data/gliders/bad_metadata.nc'),
+    'glider_std': resource_filename('cc_plugin_glider', 'tests/data/gliders/IOOS_Glider_NetCDF_v2.0.nc'),  # noqa
+    'bad_location': resource_filename('cc_plugin_glider', 'tests/data/gliders/bad_location.nc'),  # noqa
+    'bad_qc': resource_filename('cc_plugin_glider', 'tests/data/gliders/bad_qc.nc'),  # noqa
+    'bad_metadata': resource_filename('cc_plugin_glider', 'tests/data/gliders/bad_metadata.nc'),  # noqa
 }
+
 
 class TestGliderCheck(unittest.TestCase):
     # @see
@@ -16,7 +25,8 @@ class TestGliderCheck(unittest.TestCase):
     def shortDescription(self):
         return None
 
-    # override __str__ and __repr__ behavior to show a copy-pastable nosetest name for ion tests
+    # Override __str__ and __repr__ behavior to show a copy-pastable
+    # nosetest name for ion tests.
     #  ion.module:TestClassName.test_function_name
     def __repr__(self):
         name = self.id()
@@ -24,9 +34,9 @@ class TestGliderCheck(unittest.TestCase):
         if name[0] not in ["ion", "pyon"]:
             return "%s (%s)" % (name[-1], '.'.join(name[:-1]))
         else:
-            return "%s ( %s )" % (name[-1], '.'.join(name[:-2]) + ":" + '.'.join(name[-2:]))
+            return "%s ( %s )" % (name[-1], '.'.join(name[:-2]) + ":" + '.'.join(name[-2:]))  # noqa
     __str__ = __repr__
-    
+
     def get_dataset(self, nc_dataset):
         '''
         Return a pairwise object for the dataset
@@ -35,10 +45,10 @@ class TestGliderCheck(unittest.TestCase):
             nc_dataset = Dataset(nc_dataset, 'r')
             self.addCleanup(nc_dataset.close)
         return nc_dataset
-    
+
     def setUp(self):
         self.check = GliderCheck()
-    
+
     def test_location(self):
         '''
         Checks that a file with the proper lat and lon do work
@@ -53,7 +63,7 @@ class TestGliderCheck(unittest.TestCase):
         '''
         dataset = self.get_dataset(static_files['bad_location'])
         result = self.check.check_locations(dataset)
-        self.assertEquals(result.value, (0,1))
+        self.assertEqual(result.value, (0, 1))
 
     def test_ctd_fail(self):
         '''
@@ -61,15 +71,15 @@ class TestGliderCheck(unittest.TestCase):
         '''
         dataset = self.get_dataset(static_files['bad_qc'])
         result = self.check.check_ctd_variables(dataset)
-        self.assertEquals(result.value, (55,56))
-    
+        self.assertEqual(result.value, (55, 56))
+
     def test_ctd_vars(self):
         '''
         Ensures the ctd checks for the correct file
         '''
         dataset = self.get_dataset(static_files['glider_std'])
         result = self.check.check_ctd_variables(dataset)
-        self.assertEquals(result.value, (56,56))
+        self.assertEqual(result.value, (56, 56))
 
     def test_global_fail(self):
         '''
@@ -77,7 +87,7 @@ class TestGliderCheck(unittest.TestCase):
         '''
         dataset = self.get_dataset(static_files['bad_qc'])
         result = self.check.check_global_attributes(dataset)
-        self.assertEquals(result.value, (41,64))
+        self.assertEqual(result.value, (41, 64))
 
     def test_global(self):
         '''
@@ -85,8 +95,7 @@ class TestGliderCheck(unittest.TestCase):
         '''
         dataset = self.get_dataset(static_files['glider_std'])
         result = self.check.check_global_attributes(dataset)
-        self.assertEquals(result.value, (64,64))
-
+        self.assertEqual(result.value, (64, 64))
 
     def test_metadata(self):
         '''
@@ -94,7 +103,7 @@ class TestGliderCheck(unittest.TestCase):
         '''
         dataset = self.get_dataset(static_files['bad_metadata'])
         result = self.check.check_global_attributes(dataset)
-        self.assertEquals(result.value, (41,64))
+        self.assertEqual(result.value, (41, 64))
 
     def test_standard_names(self):
         '''
@@ -103,8 +112,8 @@ class TestGliderCheck(unittest.TestCase):
 
         dataset = self.get_dataset(static_files['bad_metadata'])
         result = self.check.check_standard_names(dataset)
-        self.assertEquals(result.value, (0, 1))
-        
+        self.assertEqual(result.value, (0, 1))
+
         dataset = self.get_dataset(static_files['glider_std'])
         result = self.check.check_standard_names(dataset)
-        self.assertEquals(result.value, (1, 1))
+        self.assertEqual(result.value, (1, 1))
--- setup.py
+++ setup.py
@@ -1,9 +1,39 @@
 from __future__ import with_statement
+
+import os
 import sys
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
-from cc_plugin_glider import __version__
+
+def extract_version(module='cc_plugin_glider'):
+    version = None
+    fdir = os.path.dirname(__file__)
+    fnme = os.path.join(fdir, module, '__init__.py')
+    with open(fnme) as fd:
+        for line in fd:
+            if (line.startswith('__version__')):
+                _, version = line.split('=')
+                # Remove quotation characters.
+                version = version.strip()[1:-1]
+                break
+    return version
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        # Import here, cause outside the eggs aren't loaded.
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 def readme():
     with open('README.md') as f:
@@ -11,29 +41,25 @@ def readme():
 
 reqs = [line.strip() for line in open('requirements.txt')]
 
-setup(name                 = "cc-plugin-glider",
-    version              = __version__,
-    description          = "Compliance Checker Glider DAC plugin",
-    long_description     = readme(),
-    license              = 'Apache License 2.0',
-    author               = "Luke Campbell",
-    author_email         = "lcampbell@asascience.com",
-    url                  = "https://github.com/ioos/compliance-checker",
-    packages             = find_packages(),
-    install_requires     = reqs,
-    classifiers          = [
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: Apache Software License',
-            'Operating System :: POSIX :: Linux',
-            'Programming Language :: Python',
-            'Topic :: Scientific/Engineering',
-        ],
-    entry_points         = {
-        'compliance_checker.suites': [
-            'gliderdac = cc_plugin_glider.glider_dac:GliderCheck',
-        ]
-    }
-)
-
+setup(name="cc-plugin-glider",
+      version=extract_version(),
+      description="Compliance Checker Glider DAC plugin",
+      long_description=readme(),
+      license='Apache License 2.0',
+      author="Luke Campbell",
+      author_email="lcampbell@asascience.com",
+      url="https://github.com/ioos/compliance-checker",
+      packages=find_packages(),
+      install_requires=reqs,
+      tests_require=['pytest'],
+      cmdclass=dict(test=PyTest),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering'],
+      entry_points={'compliance_checker.suites':
+                    ['gliderdac = cc_plugin_glider.glider_dac:GliderCheck']})
