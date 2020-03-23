42130680543628bcee936f28a5f0833ee15407d3
Benjamin Adams <ben.adams@rpsgroup.com>
2020-03-23 16:41:56 -0400

    Add long_description_content_type for setup.py compatibility

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     version=versioneer.get_version(),
     description="Compliance Checker Glider DAC plugin",
     long_description=readme(),
+    long_description_content_type="text/markdown",
     license='Apache License 2.0',
     author="Robert Fratantonio",
     author_email="robert.fratantonio@rpsgroup.com",
