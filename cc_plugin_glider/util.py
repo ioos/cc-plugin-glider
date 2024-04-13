3e30fea7cecfcb13ef7f3b2dcf346c102bcb9ed1
Rob Cermak <rob.cermak@gmail.com>
2024-04-13 11:43:22 -0700

    Updates

--- cc_plugin_glider/util.py
+++ cc_plugin_glider/util.py
@@ -40,7 +40,7 @@ def _check_dtype(dataset, var_name):
         if not compare_dtype(var.dtype, np.dtype(expected_dtype)):
             messages.append(
                 f"Variable {var_name} is expected to have a dtype of "
-                f"{var.dtype}, instead has a dtype of {expected_dtype}"
+                f"{expected_dtype}, instead has a dtype of {var.dtype}"
                 "",
             )
             score -= 1
@@ -57,7 +57,7 @@ def _check_dtype(dataset, var_name):
     return (score, out_of, messages)
 
 
-def _check_variable_attrs(dataset, var_name, required_attributes=None):
+def _check_variable_attrs(dataset, var_name, required_attributes=None, options=set()):
     """
     Convenience method to check a variable attributes based on the
     expected_vars dict
@@ -73,6 +73,8 @@ def _check_variable_attrs(dataset, var_name, required_attributes=None):
 
     # Get the expected attrs to check
     check_attrs = required_attributes or required_var_attrs.get(var_name, {})
+    if 'ancillary_variables' in check_attrs and 'no_ancillary_variables' in options:
+        del check_attrs['ancillary_variables']
     var_attrs = set(var.ncattrs())
     for attr in check_attrs:
         if attr == "dtype":
