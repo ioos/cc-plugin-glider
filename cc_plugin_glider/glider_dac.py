4ae3719fc87ea01ea2179c93b505e5f6d1cc18e3
Benjamin Adams <benjamin.adams@tetratech.com>
2024-10-23 21:39:21 -0400

    Change NCEI authority table URL

--- cc_plugin_glider/glider_dac.py
+++ cc_plugin_glider/glider_dac.py
@@ -89,7 +89,7 @@ class GliderCheck(BaseNCCheck):
 
         elif backup_resource is None or fail_flag:
             try:
-                resp = requests.get(url, timeout=10)
+                resp = requests.get(url, allow_redirects=True, timeout=10)
                 resp.raise_for_status()
                 text_contents = resp.text
             except RequestException:
